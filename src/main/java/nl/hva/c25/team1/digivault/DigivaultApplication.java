8986930d67a740b050dcb889fb67aa9d18080aa1
Anthon van Dijk <anthon.van.dijk@hva.nl>
2021-11-30 13:41:14 +0100

    Big bang

---
+++ src/main/java/nl/hva/c25/team1/digivault/DigivaultApplication.java
@@ -0,0 +1,13 @@
+package nl.hva.c25.team1.digivault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigivaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigivaultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/hva/c25/team1/digivault/DigivaultApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.hva.c25.team1.digivault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigivaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
