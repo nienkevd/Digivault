eae99ba40ccef636a7e74c4dac004be9a6070bc1
Erwin Kroeze <erwin.kroeze@hva.nl>
2021-12-01 10:06:59 +0100

    Aanmaak Enums AssetNaam en AssetAfkorting.

--- src/main/java/nl/hva/c25/team1/digivault/model/AssetNaam.java
+++ src/main/java/nl/hva/c25/team1/digivault/model/AssetNaam.java
@@ -2,28 +2,39 @@ package nl.hva.c25.team1.digivault.model;
 
 /**
  * @Author: Erwin, studentnummer 500889293
- * Enum met de namen van de top 20 populairste assets
+ * Enum met de namen van de top 20 populairste assets, peildatum 1 december 2021
  */
 
 public enum AssetNaam {
-    BITCOIN(),
-    ETHEREUM(),
-    BINANCE_COIN(),
-    SOLANA(),
-    CARDANO(),
-    XRP(),
-    POLKADOT(),
-    DOGECOIN(),
-    AVALANCHE(),
-    TERRA(),
-    LITECOIN(),
-    POLYGON(),
-    ALGORAND(),
-    BITCOIN_CASH(),
-    STELLAR(),
-    VECHAIN(),
-    ELROND(),
-    INTERNET_COMPUTER(),
-    TERRA_USD(),
-    FILECOIN();
+    BITCOIN("Bitcoin (BTC)"),
+    ETHEREUM("Ethereum (ETH)"),
+    BINANCE_COIN("Binance Coin (BNB)"),
+    SOLANA("Solana (SOL)"),
+    CARDANO("Cardano (ADA)"),
+    XRP("XRP (XRP)"),
+    POLKADOT("Polkadot (DOT)"),
+    DOGECOIN("Dogecoin (DOGE)"),
+    AVALANCHE("Avalanche (AVAX)"),
+    TERRA("Terra (LUNA)"),
+    LITECOIN("Litecoin (LTC)"),
+    POLYGON("Polygon (MATIC)"),
+    ALGORAND("Algorand (ALGO)"),
+    BITCOIN_CASH("Bitcoin Cash (BCH)"),
+    STELLAR("Stellar (XLM)"),
+    VECHAIN("VeChain (VET)"),
+    INTERNET_COMPUTER("Internet Computer (ICP)"),
+    TERRA_USD("TerraUSD (UST)"),
+    ELROND("Elrond (EGLD)"),
+    FILECOIN("Filecoin (FIL)");
+
+    private String naamAlsString;
+
+    AssetNaam(String naamAlsString) {
+        this.naamAlsString = naamAlsString;
+    }
+
+    @Override
+    public String toString() {
+        return naamAlsString;
+    }
 }
