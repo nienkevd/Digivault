516f03e90364e76dc886e1b9b753ce0ca24ee479
Anthon van Dijk <anthon.van.dijk@hva.nl>
2021-12-01 08:22:10 +0100

    model Portefeuille aangemaakt

---
+++ src/main/java/nl/hva/c25/team1/digivault/model/Portefeuille.java
@@ -0,0 +1,37 @@
+// Created by antho
+// Creation date 1-12-2021
+
+package nl.hva.c25.team1.digivault.model;
+
+import java.util.*;
+
+public class Portefeuille {
+/*
+    private Map<Asset, Double> assetLijst;
+
+    private Portefeuille(Map<Asset, Double> assetLijst) {
+        super();
+        this.assetLijst = assetLijst;
+    }
+
+    public Portefeuille() {
+        this(new HashMap<Asset, Double>());
+    }
+
+    public Map<Asset, Double> getAssetLijst() {
+        return assetLijst;
+    }
+
+    public void setAssetLijst(Map<Asset, Double> assetLijst) {
+        this.assetLijst = assetLijst;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder(String.format("Portefeuille:\n"));
+        for (Asset asset: assetLijst.keySet()) stringBuilder.append(String.format("%-25s%+15.5f", asset,
+                assetLijst.get(asset)));
+        return stringBuilder.append("\n").toString();
+    }
+*/
+}
