27c37184d4df720ddce67f10ae7b4c36c492e41b
Anneke van der Vliet <anneke.van.der.vliet@hva.nl>
2021-12-03 15:29:20 +0100

    kleine aanpassing in test

--- src/test/java/nl/hva/c25/team1/digivault/service/KlantServiceTest.java
+++ src/test/java/nl/hva/c25/team1/digivault/service/KlantServiceTest.java
@@ -3,9 +3,11 @@ package nl.hva.c25.team1.digivault.service;
 import nl.hva.c25.team1.digivault.model.Klant;
 import nl.hva.c25.team1.digivault.repository.JdbcKlantDAO;
 import nl.hva.c25.team1.digivault.repository.KlantDAO;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDate;
 
@@ -14,34 +16,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class KlantServiceTest {
 
-    @Test
-    void bewaarKlant() {
-        Klant nieuweKlant = new Klant(6, "122334778", LocalDate.parse("1985-12-07"));
-        JdbcKlantDAO mockDAO = Mockito.mock(JdbcKlantDAO.class);
-    }
+    @MockBean
+    private static JdbcKlantDAO mockDAO = Mockito.mock(JdbcKlantDAO.class);
+    KlantService serviceUnderTest = new KlantService(mockDAO);
+
 
     @Test
     void vindKlantOpKlantID() {
         Klant expected = new Klant(5,"132456789", LocalDate.parse("1955-01-01"));
-        JdbcKlantDAO mockDAO = Mockito.mock(JdbcKlantDAO.class);
         Mockito.when(mockDAO.vindKlantOpKlantId(5)).thenReturn(expected);
 
-        KlantService serviceUnderTest = new KlantService(mockDAO);
         Klant actual = serviceUnderTest.vindKlantOpKlantID(5);
         assertThat(actual).isNotNull().isEqualTo(expected);
     }
 
     @Test
-    void vindAlleKlanten() {
-    }
-
-    @Test
     void updateKlant() {
         Klant bestaandeKlant = new Klant(5,"132456789", LocalDate.parse("1955-02-01"));
-        JdbcKlantDAO mockDAO = Mockito.mock(JdbcKlantDAO.class);
         Mockito.when(mockDAO.vindKlantOpKlantId(5)).thenReturn(bestaandeKlant);
 
-        KlantService serviceUnderTest = new KlantService(mockDAO);
         String actual = serviceUnderTest.updateKlant(bestaandeKlant);
         assertThat(actual).contains("geslaagd");
     }
