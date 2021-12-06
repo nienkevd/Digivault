abc988eef9cb7002feea3adeb3c7435d4d13dcc9
Anneke van der Vliet <anneke.van.der.vliet@hva.nl>
2021-12-06 16:37:11 +0100

    opzet registreerKlant in rootrepo

--- src/main/java/nl/hva/c25/team1/digivault/repository/RootRepository.java
+++ src/main/java/nl/hva/c25/team1/digivault/repository/RootRepository.java
@@ -25,5 +25,34 @@ public class RootRepository {
 
     // TODO: bij registreer klant moet de methode public int slaLegePortefeuilleOpMetSleutel() worden gebruikt
 
+/*
+methode slaKlantOp(Klant klant--> meegegeven vanuit Postman/Frontend){
+
+!!BewaarmetKey geeft geen int terug maar hele object!!
+
+Klant nieuweKlant = klantDAO.bewaarKlantMetKey --> key wordt in methode in jdbc al gezet)
+
+Naam naam = naamDAO.bewaarNaamMetKey --> (nieuweKlant.setNaam(naam))
+Adres adres = adresDAO.bewaarAdresMetKey --> (nieuweKlant.setAdres(adres))
+
+accountDAO.updateKlant(klantId) --> email en wachtwoord toevoegen aan klant
+
+Rekening rekening = new Rekening(new IBAN, STARTSALDO)
+rekeningDAO.bewaarRekeningMetKey(rekening)
+nieuweKlant.setRekening
+
+nieuwe lege portefeuille:
+new Portefeuille portefeuille (standaard Map met de 20 assets en aantal op 0)?
+klant.setPortefeuille(portefeuille)
+
+om goed in database te zetten:
+
+for each door assetlijst
+portefeuilleDAO.slaItemOp(Klant, Asset, 0)
+klant.getPortefeuille.setTotaleWaarde --> 0
+
+}
+ */
+
 
 }
