eb5b2ab5b0939ef72d9095bb425126a13b29d850
Anthon van Dijk <anthon.van.dijk@hva.nl>
2021-12-07 21:04:08 +0100

    h2db files in resources

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,5 @@
 CREATE SCHEMA `digivault`;
 
-CREATE TABLE `digivault`.`rekening` (
-  `rekeningId` INT NOT NULL AUTO_INCREMENT,
-  `iban` CHAR(18) NOT NULL,
-  `saldo` DECIMAL(10,2) NOT NULL,
-  PRIMARY KEY (`rekeningId`),
-  UNIQUE INDEX `iban_UNIQUE` (`iban` ASC) VISIBLE);
-
 CREATE TABLE `digivault`.`naam` (
   `naamId` INT NOT NULL AUTO_INCREMENT,
   `voornaam` VARCHAR(45) NOT NULL,
@@ -14,11 +7,27 @@ CREATE TABLE `digivault`.`naam` (
   `achternaam` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`naamId`));
 
-CREATE TABLE `digivault`.`ziphuisnr` (
-  `ziphuisnrId` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE `digivault`.`adres` (
+  `adresId` INT NOT NULL AUTO_INCREMENT,
   `straat` VARCHAR(45) NOT NULL,
+  `huisnummer` INT NOT NULL,
+  `toevoeging` VARCHAR(45) NULL,
+  `postcode` CHAR(6) NOT NULL,
   `woonplaats` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`ziphuisnrId`));
+  PRIMARY KEY (`adresId`));
+
+CREATE TABLE `digivault`.`rekening` (
+  `rekeningId` INT NOT NULL AUTO_INCREMENT,
+  `iban` CHAR(18) NOT NULL,
+  `saldo` DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (`rekeningId`),
+  UNIQUE INDEX `iban_UNIQUE` (`iban` ASC) VISIBLE);
+
+CREATE TABLE `digivault`.`account` (
+  `accountId` INT NOT NULL AUTO_INCREMENT,
+  `emailadres` VARCHAR(45) NOT NULL,
+  `wachtwoord` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`accountId`));
 
 CREATE TABLE `digivault`.`asset` (
   `assetId` INT NOT NULL AUTO_INCREMENT,
@@ -28,15 +37,6 @@ CREATE TABLE `digivault`.`asset` (
   PRIMARY KEY (`assetId`),
   UNIQUE INDEX `naam_UNIQUE` (`naam` ASC) VISIBLE,
   UNIQUE INDEX `afkorting_UNIQUE` (`afkorting` ASC) VISIBLE);
-
-CREATE TABLE `digivault`.`adres` (
-  `adresId` INT NOT NULL AUTO_INCREMENT,
-  `postcode` CHAR(6) NOT NULL,
-  `huisnummer` INT NOT NULL,
-  `toevoeging` VARCHAR(45) NULL,
-  `ziphuisnrId` INT NOT NULL,
-  PRIMARY KEY (`adresId`),
-  INDEX `verzinzelf1_idx` (`ziphuisnrId` ASC) VISIBLE);
 
 CREATE TABLE `digivault`.`eurokoers` (
   `eurokoersId` INT NOT NULL AUTO_INCREMENT,
@@ -50,17 +50,16 @@ CREATE TABLE `digivault`.`klant` (
   `klantId` INT NOT NULL AUTO_INCREMENT,
   `bsn` CHAR(9) NOT NULL,
   `geboortedatum` DATE NOT NULL,
-  `emailadres` VARCHAR(45) NOT NULL,
-  `wachtwoord` VARCHAR(45) NOT NULL,
   `naamId` INT NOT NULL,
   `adresId` INT NOT NULL,
   `rekeningId` INT NOT NULL,
+  `accountId` INT NOT NULL,
   PRIMARY KEY (`klantId`),
   UNIQUE INDEX `bsn_UNIQUE` (`bsn` ASC) VISIBLE,
   INDEX `verzinzelf2_idx` (`naamId` ASC) VISIBLE,
   INDEX `verzinzelf3_idx` (`adresId` ASC) VISIBLE,
   INDEX `verzinzelf4_idx` (`rekeningId` ASC) VISIBLE,
-  UNIQUE INDEX `emailadres_UNIQUE` (`emailadres` ASC) VISIBLE);
+  INDEX `verzinzelf1_idx` (`accountId` ASC) VISIBLE);
 
 CREATE TABLE `digivault`.`portefeuille_item` (
   `itemId` INT NOT NULL AUTO_INCREMENT,
