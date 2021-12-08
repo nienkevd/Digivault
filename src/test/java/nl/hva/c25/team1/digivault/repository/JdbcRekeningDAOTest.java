ab580e88207b9cf852deeadd2a33f27459e2ed44
Nienke van Dijk <nienke.van.dijk@hva.nl>
2021-12-08 17:46:55 +0100

    test JDBC+pom+application-test

--- src/test/java/nl/hva/c25/team1/digivault/repository/JdbcRekeningDAOTest.java
+++ src/test/java/nl/hva/c25/team1/digivault/repository/JdbcRekeningDAOTest.java
@@ -1,15 +1,31 @@
 package nl.hva.c25.team1.digivault.repository;
 
+import nl.hva.c25.team1.digivault.model.Rekening;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class JdbcRekeningDAOTest {
+   // private static Rekening expected = new Rekening(1,"AA00ABCD0000000000");
+    private RekeningDAO daoUnderTest;
 
+    @Autowired
+    public JdbcRekeningDAOTest(RekeningDAO daoUnderTest) {
+        this.daoUnderTest = daoUnderTest;
+    }
+
+    @Test
+    public void setupTest() {
+        assertThat(daoUnderTest).isNotNull();
+    }
 
     @Test
     void bewaar() {
@@ -25,6 +41,9 @@ class JdbcRekeningDAOTest {
 
     @Test
     void vindRekeningOpIBAN() {
+        Rekening actual = daoUnderTest.vindRekeningOpIBAN("AA00ABCD0000000000");
+        Rekening expected = new Rekening(1,"AA00ABCD0000000000");
+        assertThat(expected).isEqualTo(actual);
     }
 
     @Test
