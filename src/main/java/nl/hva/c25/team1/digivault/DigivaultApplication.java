25f97044d0c441a87b0eb99049b76f8878504ec5
Anthon van Dijk <anthon.van.dijk@hva.nl>
2021-12-14 16:13:01 +0100

    security bug fixed?

--- src/main/java/nl/hva/c25/team1/digivault/DigivaultApplication.java
+++ src/main/java/nl/hva/c25/team1/digivault/DigivaultApplication.java
@@ -2,8 +2,9 @@ package nl.hva.c25.team1.digivault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class DigivaultApplication {
 
     public static void main(String[] args) {
