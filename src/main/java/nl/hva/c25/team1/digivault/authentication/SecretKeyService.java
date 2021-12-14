1a69b7ce98626f3f971f5217e4eb2b36fcf98056
Anneke van der Vliet <anneke.van.der.vliet@hva.nl>
2021-12-14 22:15:42 +0100

    JWT service maakJWT, authenticateJWT en getClaim poging 1

---
+++ src/main/java/nl/hva/c25/team1/digivault/authentication/SecretKeyService.java
@@ -0,0 +1,10 @@
+package nl.hva.c25.team1.digivault.authentication;
+
+public class SecretKeyService {
+
+    private static final String SECRET = "ItsFunToHaveASecretAndKeepItVeryWell";
+
+    public String getSecret() {
+        return SECRET;
+    }
+}
--- src/main/java/nl/hva/c25/team1/digivault/service/JWTService.java
+++
@@ -1,7 +0,0 @@
-package nl.hva.c25.team1.digivault.service;
-
-public class JWTService {
-//    Algorithm algorithmHS = Algorithm.HMAC256("secret");
-
-
-}
