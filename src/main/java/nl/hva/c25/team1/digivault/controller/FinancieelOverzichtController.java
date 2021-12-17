ca009d07776a1d85d4e9511cbc592184dfad5401
Anthon van Dijk <anthon.van.dijk@hva.nl>
2021-12-17 16:05:26 +0100

    authorization klaar

--- src/main/java/nl/hva/c25/team1/digivault/controller/FinancieelOverzichtController.java
+++ src/main/java/nl/hva/c25/team1/digivault/controller/FinancieelOverzichtController.java
@@ -2,6 +2,7 @@ package nl.hva.c25.team1.digivault.controller;
 
 import nl.hva.c25.team1.digivault.authentication.TokenService;
 import nl.hva.c25.team1.digivault.model.FinancieelOverzicht;
+import nl.hva.c25.team1.digivault.service.AccountService;
 import nl.hva.c25.team1.digivault.service.FinancieelOverzichtService;
 import org.springframework.boot.autoconfigure.cassandra.CassandraProperties;
 import org.springframework.http.HttpStatus;
@@ -24,15 +25,18 @@ public class FinancieelOverzichtController {
 
     private FinancieelOverzichtService financieelOverzichtService;
     private TokenService tokenService;
+    private AccountService accountService;
 
     /**
      * Constructor van de FinancieelOverzichtController
      * @param financieelOverzichtService FinancieelOverzichtService
      */
-    public FinancieelOverzichtController(FinancieelOverzichtService financieelOverzichtService, TokenService tokenService) {
+    public FinancieelOverzichtController(FinancieelOverzichtService financieelOverzichtService,
+                                         TokenService tokenService, AccountService accountService) {
         super();
         this.financieelOverzichtService = financieelOverzichtService;
         this.tokenService = tokenService;
+        this.accountService = accountService;
     }
 
     /**
@@ -43,8 +47,9 @@ public class FinancieelOverzichtController {
     @GetMapping("/financieeloverzicht/{klantId}")
     public ResponseEntity<FinancieelOverzicht> vindFinancieelOverzicht(@PathVariable int klantId,
                                                                        @RequestHeader("Authorization") String token) {
-        boolean authorized = tokenService.getEmailadresToken(token).equals("");
-        if (tokenService.valideerJWT(token) || authorized) {
+        boolean authorized = tokenService.getEmailadresToken(token).equals(accountService.vindAccountOpKlantId(klantId).
+                getEmailadres());
+        if (tokenService.valideerJWT(token) && authorized) {
             return ResponseEntity.ok(financieelOverzichtService.genereerFinancieelOverzicht(klantId));
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
