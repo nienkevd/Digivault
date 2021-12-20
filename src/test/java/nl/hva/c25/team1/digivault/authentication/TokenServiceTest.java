206cd7dd5cfca459782a3cac63ff46c4c844d7a5
Anneke van der Vliet <anneke.van.der.vliet@hva.nl>
2021-12-20 12:43:15 +0100

    TokenServiceTest 2 testen gemaakt

--- src/test/java/nl/hva/c25/team1/digivault/authentication/TokenServiceTest.java
+++ src/test/java/nl/hva/c25/team1/digivault/authentication/TokenServiceTest.java
@@ -4,71 +4,46 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import nl.hva.c25.team1.digivault.repository.JdbcKlantDAO;
-import nl.hva.c25.team1.digivault.service.KlantService;
 import org.apache.commons.codec.binary.Base64;
-import org.assertj.core.util.Maps;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
-import org.springframework.boot.test.mock.mockito.MockBean;
+
 
 import java.nio.charset.StandardCharsets;
-import java.util.Map;
-import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.jupiter.api.Assertions.*;
 
 
 
 class TokenServiceTest {
 
-    private TokenService tokenServiceTest;
-    String testToken;
-
+    String testJWT;
+    private final String KEY = SecretKeyService.getSecret();
+    private SecretKeyService secretKeyService;
+    private TokenService tokenServiceTest = new TokenService(secretKeyService);
+    private final String ISSUER = "Digivault";
+    private final String TEST_EMAIL = "anneke@hva.nl";
 
 
     @Test
-    void maakEenLegeHMAC512SignedToken() throws Exception {
-        String signed = JWT.create().sign(Algorithm.HMAC256("secret"));
+    void maakEenLegeHMAC512SignedToken() {
+        String signed = JWT.create().sign(Algorithm.HMAC256(KEY));
         assertThat(signed).isNotNull();
         String[] parts = signed.split("\\.");
         String headerJson = new String(Base64.decodeBase64(parts[0]), StandardCharsets.UTF_8);
         assertThat(headerJson).contains("alg", "HS256");
         assertThat(headerJson).contains("typ", "JWT");
-        JWTVerifier verified = JWT.require(Algorithm.HMAC256("secret"))
+        JWTVerifier verified = JWT.require(Algorithm.HMAC256(KEY))
                 .build();
         assertThat(verified).isNotNull();
     }
 
-//    @Test
-//    void valideerAccessTokenMetVerkeerdEmailadres() {
-//        testToken= tokenServiceTest.maakJWT("anneke@hva.nl");
-//        boolean returnedBooleanValue = tokenServiceTest.valideerJWT(testToken);
-//        // print alle eigenschappen van het token
-//        DecodedJWT decodedJwt = JWT.decode(testToken);
-////        printOutComesTokenValidation(decodedJwt);
-//        // als de boolean false is is de test geslaagd
-//        if(!returnedBooleanValue){
-//            System.out.println("test geslaagd");
-//        } else {
-//            fail("Moet een JWTVerificationException gooien met InvalidClaimException en false teruggeven");
-//        }
-//    }
-
-
-
     @Test
-    void maakJWTTest(){
-
-
+    void maakVolledigTokenEnCheckInhoud() {
+        testJWT = tokenServiceTest.maakJWT(TEST_EMAIL);
+        DecodedJWT decodedJWT = JWT.decode(testJWT);
+        assertThat(decodedJWT).isNotNull();
+        assertThat(decodedJWT.getClaims().get("emailadres").asString()).isEqualTo(TEST_EMAIL);
+        assertThat(decodedJWT.getIssuer()).isEqualTo(ISSUER);
     }
 
-
-
-    @Test
-    void valideerJWT() {
-    }
 }
