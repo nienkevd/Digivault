bd4da034d072161ec2049a3f2fb413723d47da91
Erwin Kroeze <erwin.kroeze@hva.nl>
2021-12-24 13:11:04 +0100

    Update CSS, toevoeging fonts, aanpassing registratieLaag.

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,8 @@
+<!-- Tijdelijk javascript om de registratieLaag te tonen -->
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
