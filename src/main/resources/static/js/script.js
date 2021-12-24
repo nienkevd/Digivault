798ca8c7c426fd51709eb690d50ccbeb22e6245d
Erwin Kroeze <erwin.kroeze@hva.nl>
2021-12-24 15:47:36 +0100

    Toevoegen Diva-effect buttons voor extra vakantievreugde.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -5,14 +5,3 @@ function on() {
 function off() {
     document.getElementById("overlay").style.display = "none";
 }
-
-$('#show_password').hover(function functionName() {
-        //Change the attribute to text
-        $('#password').attr('type', 'text');
-        $('.glyphicon').removeClass('glyphicon-eye-open').addClass('glyphicon-eye-close');
-    }, function () {
-        //Change the attribute back to password
-        $('#password').attr('type', 'password');
-        $('.glyphicon').removeClass('glyphicon-eye-close').addClass('glyphicon-eye-open');
-    }
-);
