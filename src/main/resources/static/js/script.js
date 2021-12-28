e47737b23bf65130c28e4f85fa627c3f84877736
Erwin Kroeze <erwin.kroeze@hva.nl>
2021-12-28 23:15:19 +0100

    Toevoegen check op overeenkomst ww in registratieLaag.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -7,3 +7,13 @@ function on() {
 function off() {
     document.getElementById("overlay").style.display = "none";
 }
+
+<!-- Check op overeenkomen wachtwoord in registratieLaag -->
+$('#wachtwoord_reg, #wachtwoord_check').on('keyup', function () {
+    if ($('#wachtwoord_reg').val().length === 0 || $('#wachtwoord_check').val().length === 0) {
+        $('#wachtwoordInfo').html('').css('color', 'black');
+    } else if ($('#wachtwoord_reg').val() === $('#wachtwoord_check').val()) {
+        $('#wachtwoordInfo').html('komt overeen').css('color', 'green');
+    } else
+        $('#wachtwoordInfo').html('niet identiek').css('color', '#C0392B');
+});
