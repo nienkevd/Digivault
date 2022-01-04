b44d3d1ae17c91f27d49ed169093448765a6e6ac
Erwin Kroeze <erwin.kroeze@hva.nl>
2022-01-04 22:47:29 +0100

    Vervangen script jQuery, update frontend registratielaag.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,14 +1,14 @@
 "use strict";
 
-<!-- Tijdelijk javascript om de registratieLaag te tonen -->
-function on() {
+// Tijdelijk javascript om de registratieLaag te tonen
+function registratieLaagAan() {
     document.getElementById("overlay").style.display = "block";
 }
-function off() {
+function registratieLaagUit() {
     document.getElementById("overlay").style.display = "none";
 }
 
-<!-- Check op overeenkomen wachtwoord in registratieLaag -->
+// Check op overeenkomst wachtwoorden in registratieLaag
 $('#wachtwoord_reg, #wachtwoord_check').on('keyup', function () {
     if ($('#wachtwoord_reg').val().length === 0 || $('#wachtwoord_check').val().length === 0) {
         $('#wachtwoordInfo').html('').css('color', 'black');
@@ -17,3 +17,7 @@ $('#wachtwoord_reg, #wachtwoord_check').on('keyup', function () {
     } else
         $('#wachtwoordInfo').html('niet identiek').css('color', '#C0392B');
 });
+
+// Ik heb een reCHAPTA v2 aanvraag gedaan voor ons domein team-4, zie developers.google.com/recaptcha:
+//  site key:   6Ld85NcdAAAAALDUx4Wl943frenGbzW_7Zx4QQDH
+//  secret key: 6Ld85NcdAAAAAJ8m6eosIuvmNPbB58zG3Tv8hz6v
