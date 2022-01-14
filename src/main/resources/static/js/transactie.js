9e9e00542499dbe1496c7f6cc482c12027e4fef2
Sezi <sezi.taser@hva.nl>
2022-01-14 10:35:54 +0100

    saldo js

---
+++ src/main/resources/static/js/transactie.js
@@ -0,0 +1,63 @@
+fetch("http://localhost:8080/financieeloverzicht/10", {
+    method: 'GET',
+    headers: {
+        'Authorization': 'Bearer ' /*+ localStorage.getItem("token")*/,
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+    },
+})
+    .then(response => {
+        if(response.status === 200){
+            return response.json()
+        } else if(response.status === 401){
+            console.log(response.headers.get('Location'))
+            window.location.href = response.headers.get('Location');
+        }
+    })
+    .then(json =>
+        toonSaldo(json)
+    )
+    .catch((err) => {
+        console.log(err);
+    });
+
+function toonSaldo (json) {
+    const saldo = document.getElementById("saldo")
+    saldo.append(json.saldo);
+}
+
+// asset.addEventListener('click', function (e) {
+//     toonWaarde();
+//     toonTransactieKosten();
+// })
+//
+// document.getElementById("koop").addEventListener("click", (e)
+//     || document.getElementById("verkoop").addEventListener("click", (e) => {
+//         let hoeveelheid = document.getElementById("hoeveelheid").value;
+//         let asset = document.getElementById("dropdown").value;
+//         let data = {'hoeveelheid': hoeveelheid, 'dropdown': asset};
+//
+//         fetch("http://localhost:8080/transactie/1", {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Access-Control-Allow-Origin': '*'
+//             },
+//             body: JSON.stringify(data),
+//         })
+//             .then((response) => {
+//                 console.log(response);
+//                 return response.json()
+//             })
+//             .then((data) => {
+//                 console.log(data);
+//                 if (data.error) {
+//                     alert("Transactie niet mogelijk");
+//                 } else {
+//                     toonFinancieelOverzicht();
+//                 }
+//             })
+//             .catch((err) => {
+//                 console.log(err);
+//             })
+//     })
