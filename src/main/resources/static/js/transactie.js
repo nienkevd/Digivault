ee3ba5a140f6d56dd6a480c408b3256a201c3fda
Sezi <sezi.taser@hva.nl>
2022-01-17 14:42:31 +0100

    transactie js

--- src/main/resources/static/js/transactie.js
+++ src/main/resources/static/js/transactie.js
@@ -32,35 +32,43 @@ document.getElementById('asset').addEventListener('change', function (e) {
 })
 
 function toonWaarde (json) {
-
+    let hoeveelheid = document.getElementById("hoeveelheid").value;
+    let dagkoers = json.dagkoers;
+    const waarde = (aantal * dagkoers);
+    document.getElementById("waarde").append(waarde);
 }
-//
-// document.getElementById("koop").addEventListener("click", (e) => {
-//         let hoeveelheid = document.getElementById("hoeveelheid").value;
-//         let asset = document.getElementById("dropdown").value;
-//         let data = {'hoeveelheid': hoeveelheid, 'dropdown': asset};
-//
-//         fetch("http://localhost:8080/transactie/10", {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 'Access-Control-Allow-Origin': '*'
-//             },
-//             body: JSON.stringify(data),
-//         })
-//             .then((response) => {
-//                 console.log(response);
-//                 return response.json()
-//             })
-//             .then((data) => {
-//                 console.log(data);
-//                 if (data.error) {
-//                     alert("Transactie niet mogelijk");
-//                 } else {
-//                     toonFinancieelOverzicht();
-//                 }
-//             })
-//             .catch((err) => {
-//                 console.log(err);
-//             })
-//     })
+
+function toonTransactieKosten(json) {
+    const transactiekosten = (waarde * );
+    document.getElementById("transactiekosten").append(transactiekosten);
+}
+
+document.getElementById("koop").addEventListener("click", (e) => {
+        let hoeveelheid = document.getElementById("hoeveelheid").value;
+        let asset = document.getElementById("dropdown").value;
+        let data = {'hoeveelheid': hoeveelheid, 'dropdown': asset};
+
+        fetch("http://localhost:8080/transactie/10", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => {
+                console.log(response);
+                return response.json()
+            })
+            .then((data) => {
+                console.log(data);
+                if (data.error) {
+                    alert("Transactie niet mogelijk");
+                } else {
+                    toonFinancieelOverzicht();
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    })
