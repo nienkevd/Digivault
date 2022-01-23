727434fb569e3208931b422ab654e6ed38ac8eaa
Anthon van Dijk <anthon.van.dijk@hva.nl>
2022-01-23 11:54:08 +0100

    ik kap met testing voor ik echt gek wordt

--- src/test/java/nl/hva/c25/team1/digivault/controller/TransactieControllerTest.java
+++ src/test/java/nl/hva/c25/team1/digivault/controller/TransactieControllerTest.java
@@ -1,55 +1,51 @@
-//// gemaakt door Anthon van Dijk
-//
-//package nl.hva.c25.team1.digivault.controller;
-//
-//import nl.hva.c25.team1.digivault.model.Transactie;
-//import nl.hva.c25.team1.digivault.service.TransactieService;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.*;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.test.web.servlet.*;
-//import org.springframework.test.web.servlet.request.*;
-//import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-//
-//import java.util.List;
-//
-//@AutoConfigureMockMvc
-//@SpringBootTest
-//class TransactieControllerTest {
-//
-//    private MockMvc mockMvc;
-//
-//    @Mock
-//    private List<Transactie> transactieLijst;
-//
-//    @MockBean
-//    private TransactieService transactieService;
-//
-//    @Autowired
-//    public TransactieControllerTest(MockMvc mockMvc) {
-//        this.mockMvc = mockMvc;
-//    }
-//
-//    @Test
-//    void vindAlleTransactiesOpKoperHandler() {
-//        // geef aan wat de service als teruggeefwaarde heeft
-//        Mockito.when(transactieService.vindAlleTransactiesOpKoper(10)).thenReturn(transactieLijst);
-//        // Maak een gesimuleerde request aan
-//        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/transactie/{koper}", 10);
-//        // Stuur deze request naar de controller via de MockMvc (try-catch)
-//        try {
-//            ResultActions response = mockMvc.perform(request);
-//            response.andExpect(MockMvcResultMatchers.status().isOk());
-//        }
-//        catch(Exception e) {
-//            System.out.println(e.getMessage());
-//        }
-//
-//
-////        3. Ontvang de response.
-////        4. Test of de response overeenkomt met de verwachting.
-//    }
-//}
+// gemaakt door Anthon van Dijk
+
+package nl.hva.c25.team1.digivault.controller;
+
+import nl.hva.c25.team1.digivault.model.Transactie;
+import nl.hva.c25.team1.digivault.service.TransactieService;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.*;
+import org.springframework.test.web.servlet.request.*;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class TransactieControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private Transactie transactie;
+
+    @MockBean
+    private TransactieService transactieService;
+
+    @Autowired
+    public TransactieControllerTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    void vindTransactieOpTransactieIdHandler() {
+        // geef aan wat de service als teruggeefwaarde heeft
+        Mockito.when(transactieService.vindTransactieOpTransactieId(1)).thenReturn(transactie);
+        // Maak een gesimuleerde request aan
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/transactie/{koper}", 1);
+        // Stuur deze request naar de controller via de MockMvc (try-catch) en ontvang de response
+        try {
+            ResultActions response = mockMvc.perform(request);
+            // Test of de response overeenkomt met de verwachting
+            response.andExpect(MockMvcResultMatchers.status().isOk());
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
