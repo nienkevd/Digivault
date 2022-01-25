93ed687cf72c814d58ec21bab9722a2b1f948a8d
Anthon van Dijk <anthon.van.dijk@hva.nl>
2022-01-25 12:48:12 +0100

    test vindTransactieOpTransactieIdHandler afgerond

--- src/test/java/nl/hva/c25/team1/digivault/controller/TransactieControllerTest.java
+++ src/test/java/nl/hva/c25/team1/digivault/controller/TransactieControllerTest.java
@@ -17,6 +17,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 @WebMvcTest(TransactieController.class)
 class TransactieControllerTest {
 
@@ -44,14 +46,32 @@ class TransactieControllerTest {
     void vindTransactieOpTransactieIdHandler() {
         Transactie transactie = new Transactie(LocalDate.of(2022, 1, 25),
                 LocalTime.of(12, 12, 0), 1.5);
+
+        // service response = transactie
         Mockito.when(transactieService.vindTransactieOpTransactieId(1)).thenReturn(transactie);
-        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/transactie/1");
+        MockHttpServletRequestBuilder request1 = MockMvcRequestBuilders.get("/transactie/1");
+
+        // service response = null
+        Mockito.when(transactieService.vindTransactieOpTransactieId(2)).thenReturn(null);
+        MockHttpServletRequestBuilder request2 = MockMvcRequestBuilders.get("/transactie/2");
+
+        // ongeldige request
+        MockHttpServletRequestBuilder request3 = MockMvcRequestBuilders.get("/transactie/pipo/de/kloon");
+
         try {
-            ResultActions response = mockMvc.perform(request);
+
+            ResultActions response = mockMvc.perform(request1);
             response.andExpect(MockMvcResultMatchers.status().isOk())
                     .andExpect(MockMvcResultMatchers.content().json("{\"koper\":null,\"verkoper\":null," +
                             "\"transactieDatum\":\"2022-01-25\",\"transactieTijd\":\"12:12:00\",\"asset\":null," +
                             "\"aantalCryptos\":1.5}"));
+
+            response = mockMvc.perform(request2);
+            response.andExpect(MockMvcResultMatchers.status().isOk()).andExpect(jsonPath("$").doesNotExist());
+
+            response = mockMvc.perform(request3);
+            response.andExpect(MockMvcResultMatchers.status().isNotFound());
+
         }
         catch(Exception e) {
             System.out.println(e.getMessage());
