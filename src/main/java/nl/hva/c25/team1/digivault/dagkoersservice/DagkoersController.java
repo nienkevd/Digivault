ec8b4356e360e616d0404c07514ad8836563e155
Anneke van der Vliet <anneke.van.der.vliet@hva.nl>
2022-01-26 09:56:29 +0100

    Maintenance DagkoersService klaar

--- src/main/java/nl/hva/c25/team1/digivault/dagkoersservice/DagkoersController.java
+++ src/main/java/nl/hva/c25/team1/digivault/dagkoersservice/DagkoersController.java
@@ -24,7 +24,7 @@ public class DagkoersController {
     @CrossOrigin
     @GetMapping("/dagkoersen")
     public String geefDagkoersen() throws JsonProcessingException {
-        dagkoersService.slaDagkoersenOp();
+        dagkoersService.dagkoersBuilder();
         return "Geslaagd!";
     }
 
--- src/main/java/nl/hva/c25/team1/digivault/dagkoersservice/DagkoersService.java
+++ src/main/java/nl/hva/c25/team1/digivault/dagkoersservice/DagkoersService.java
@@ -8,13 +8,11 @@ import nl.hva.c25.team1.digivault.model.EuroKoers;
 import nl.hva.c25.team1.digivault.repository.AssetDAO;
 import nl.hva.c25.team1.digivault.repository.EuroKoersDAO;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 import java.time.LocalDate;
 
 
@@ -22,8 +20,8 @@ import java.time.LocalDate;
 @EnableScheduling
 @Service
 public class DagkoersService {
-    private EuroKoersDAO euroKoersDAO;
-    private AssetDAO assetDAO;
+    private final EuroKoersDAO euroKoersDAO;
+    private final AssetDAO assetDAO;
     public final double DOLLARKOERS = 0.88;
     public final String URL = "https://api.coinranking.com/v2/coins";
     public final String PATH = "/data/coins";
@@ -31,6 +29,8 @@ public class DagkoersService {
     public final int MAX_ASSET = 20;
     public final String SYMBOL = "symbol";
     public final String PRICE = "price";
+    public final String ZONE = "CET";
+    public final String OPHAALTIJD = "0 00 09 * * ?";
 
     public DagkoersService(EuroKoersDAO euroKoersDAO, AssetDAO assetDAO) {
         super();
@@ -38,31 +38,37 @@ public class DagkoersService {
         this.assetDAO = assetDAO;
     }
 
-    @Scheduled(cron = "0 00 09 * * ?", zone = "CET") // elke dag om 00:01 uur
-    public void slaDagkoersenOp() throws JsonProcessingException {
-        EuroKoers euroKoers = new EuroKoers();
+    @Scheduled(cron = OPHAALTIJD, zone = ZONE) // elke dag om 9 am
+    public void dagkoersBuilder() throws JsonProcessingException {
         RestTemplate restTemplate = new RestTemplate();
         ObjectMapper mapper = new ObjectMapper();
         ResponseEntity<String> response
                 = restTemplate.getForEntity(URL, String.class);
         JsonNode root = mapper.readTree(response.getBody());
         JsonNode coins = root.at(PATH);
+        bouwEuroKoersEnSlaOp(coins);
+    }
+
+    public void bouwEuroKoersEnSlaOp(JsonNode coins) {
+        EuroKoers euroKoers = new EuroKoers();
         for (int i = MIN_ASSET; i <= MAX_ASSET; i++) {
             Asset asset = assetDAO.vindAssetOpId(i);
-            if (coins.isArray()) {
-                for (JsonNode jsonNode : coins) {
-                    String afkorting = jsonNode.get(SYMBOL).asText();
-                    if ((asset.getAfkorting()).equals(afkorting)) {
-                        JsonNode dagkoers = jsonNode.get(PRICE);
-                        euroKoers.setAssetId(i);
-                        euroKoers.setEuroKoersId(i);
-                        euroKoers.setDatum(LocalDate.now());
-                        euroKoers.setKoers(dagkoers.asDouble() * DOLLARKOERS);
-                        euroKoersDAO.bewaarEuroKoersMetSK(euroKoers);
-                    }
+            for (JsonNode jsonNode : coins) {
+                String afkorting = jsonNode.get(SYMBOL).asText();
+                if ((asset.getAfkorting()).equals(afkorting)) {
+                    slaEuroKoersOp(i, euroKoers,jsonNode);
                 }
             }
         }
+    }
+
+    public void slaEuroKoersOp(int i, EuroKoers euroKoers, JsonNode jsonNode) {
+        JsonNode dagkoers = jsonNode.get(PRICE);
+        euroKoers.setAssetId(i);
+        euroKoers.setEuroKoersId(i);
+        euroKoers.setDatum(LocalDate.now());
+        euroKoers.setKoers(dagkoers.asDouble() * DOLLARKOERS);
+        euroKoersDAO.bewaarEuroKoersMetSK(euroKoers);
     }
 
     //        String accessKey = "coinrankingf65e600fd8d4915137af49c11c136d2619e87f7c49345115";
